41479a9c7910ea2188c70c8adb93e8784aee0a42
AroniasPrenovost <Aroniascoder@gmail.com>
2017-07-15 10:12:13 -0700

    added pug template engine

--- app.js
+++ app.js
@@ -7,7 +7,15 @@ var Book = require('./Book.model');
 var port = 8080;
 var db = 'mongodb://localhost/example';
 
+// set up templating engine 
+app.set('view engine', 'pug');
+path = require('path');
 
+// set views path and public path 
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// connect database 
 mongoose.connect(db);
 
 // parse json and url data
@@ -16,9 +24,19 @@ app.use(bodyParser.urlencoded({
 	extended: true
 }));
 
-// add routes for db retrieval 
+// basic GET request to hoemepage
+/*
 app.get('/', function(req, res) {
-	res.send('Hey there');
+	res.send('hey there');
+});
+*/
+// first route renders jade template
+app.get('/', function (req, res) {
+  res.render('index', { 
+    title: 'Hello!', 
+    message: 'Welcome to the cliche book store!',
+    body: 'Please, take a look around and spend some time combing through our vast collection.'
+  });
 });
 
 app.get('/books', function(req, res) {
@@ -108,7 +126,6 @@ app.delete('/book/:id', function(req, res) {
     }
   });
 });
-
 
 app.listen(port, function() {
 	console.log('app listen on port' + port);
